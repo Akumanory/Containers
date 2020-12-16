cc6457c4657913f3281a9e96738a5b4e144480f4
Akumanory <kencher87@gmail.com>
2020-12-17 02:50:00 +0400

    Done with dynamic, array need tests

--- dynamic_array.cpp
+++ dynamic_array.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <utility>
-#include <iostream>
+// #include <iostream>
 
 const int MIN_CAPACITY = 8;
 const int GROWING_FACTOR = 2;
@@ -9,6 +9,51 @@ template <class T>
 class Array final
 {
 public:
+    class Iterator {
+
+    public:
+        Iterator(Array* array_class) {
+            _array_class = array_class;
+            _position = 0;
+        }
+
+        const T& get() const {
+            return *(_array_class->_array + _position);
+        }
+
+        void set(const T& value) {
+            _array_class->_array[_position] = value;
+        }
+
+        void insert(const T& value) {
+            _array_class->insert(_position, value);
+        }
+
+        void remove() {
+            _array_class->remove(_position);
+        }
+
+        void next() {
+            _position++;
+        }
+
+        void prev() {
+            _position--;
+        }
+
+        bool hasNext() const {
+            return _array_class->size() > _position;
+        }
+
+        bool  hasPrev() const {
+            return _position != 0;
+        }
+
+    private:
+        int _position;
+        Array * _array_class;
+    };
+
     Array() {
         _array_capacity = MIN_CAPACITY;
         _array = (T*)malloc(_array_capacity * sizeof(*_array));
@@ -28,11 +73,12 @@ public:
     void insert(const T& value) {
         if (_count_of_elements >= _array_capacity)
             resize();
-        T* placement_new = new (_array + _count_of_elements) T(value);
+        new (_array + _count_of_elements) T(std::move(value));
         // *(_array + _count_of_elements) = value;
         _count_of_elements++;
     }
 
+// Убрать часть с тем что можно втавлять значения по индексу которые больше капасити
     void insert(int index, const T& value) {
         if (_count_of_elements == _array_capacity || index >= _array_capacity)
             resize();
@@ -41,55 +87,71 @@ public:
         {
             for (int i = _count_of_elements; i > index; i--)
             {
-                // *(_array + i + 1) = *(_array + i);
-                // _array + i = nullptr;
-                // *(_array + i + 1) = std::move(*(_array + i));
-                T* placement_new = new (_array + i) T(*(_array + i));
-                *(_array + i + 1) = *(placement_new);
-                placement_new->~T();
+                // std::cout << "in insert " << std::endl;
+                new (_array + i) T(std::move(*(_array + i - 1)));
             }
-            T* placement_new = new (_array + index) T(value);
+            new (_array + index) T(std::move(value));
             _count_of_elements++;
-        }else
-        {
+
+        }
+        else {
             if (index > _count_of_elements)
             {
-                T* placement_new = new (_array + index) T(value);
+                new (_array + index) T(std::move(value));
                 _count_of_elements += index -_count_of_elements + 1;
             }else
             {
                 insert(value);
             }
-            
         }
     }
 
     void remove(int index) {
-        if (index <= _count_of_elements && index >= _count_of_elements)
+        if (index <= _count_of_elements && !(index >= _count_of_elements))
         {
-            T* placement_new = new (_array + index) T();
-            placement_new->~T();
-            for (int i = index + 1; i < _count_of_elements; i++)
+            // std::cout << "remove" << std::endl;
+            // T* placement_new = new (_array + index) T();
+            // placement_new->~T();
+            (_array + index)->~T();
+
+
+            for (int i = index; i < _count_of_elements - 1; i++)
             {
-                *(_array + index) = std::move(*(_array + index));
+                // std::cout << "in remove loop" << std::endl;
+                 
+                new (_array + i) T(std::move(*(_array + i + 1)));
+                // *(_array + i) = *(_array + i + 1);
+                
+                // new (_array + i + 1) T();
             }
-            
+            _count_of_elements--;
         }
-    }
-// TEST METHODS
-    void test_resize() {
-        resize();
     }
 
-    void testm_print() {
-        for (int i = 0; i < _array_capacity; i++)
-        {
-            std::cout << *(_array + i) << " ";
-        }
-        std::cout << std::endl;
+    int size() const {
+        return _count_of_elements;
+    }
+
+    const T& operator[](int index) const {
+        return *(_array + index);
+    }
+
+    T& operator[](int index) {
+        return *(_array + index);
+    }
+
+    Iterator iterator() {
+        return Iterator(this);
+    }
+    const Iterator iterator() const {
+        return Iterator(this);
     }
 
 private:
+    // Запрещение копирования
+    Array(const Array&);
+    // Запрещение копирования
+    Array& operator=(const Array&);
     // Количество реально существующих элементов в массиве
     int _count_of_elements;
     // Количество элементов возможных для заполнения в массиве
@@ -108,6 +170,9 @@ private:
         // temp_array = std::move(_array);
         // _array = std::move(temp_array);
         T* temp_array = (T*)malloc(capacity * sizeof(*_array));
+
+        // std::cout << "temp_array " << *(temp_array) << std::endl;
+
         // DEBUG START
         // std::cout << "temp arra befor initialize" << std::endl;
         // for (int i = 0; i < capacity; i++)
@@ -115,11 +180,23 @@ private:
         //     std::cout << *(temp_array + i) << " ";
         // }
         // DEBUG END
+
+        // T* peek = _array;
+        // std::cout << "Before free and before move" << std::endl;
+        // for (int i = 0; i < _array_capacity; i++)
+        // {
+        //     std::cout << *(peek + i) << " ";
+        // }
+        // std::cout << std::endl;
         
         // std::cout << std::endl;
         for (int i = 0; i < _count_of_elements; i++)
         {
-            T* placement_new = new (temp_array + i) T(*(_array + i));
+            // move_test(temp_array + i, _array + i);
+            new (temp_array + i) T(std::move(*(_array + i)));
+
+            // T* placement_new = new (temp_array + i) T(*(_array + i));
+
             // (temp_array + i)->~T();
             // TEST START Placement new and copy constructor
             // T* placement_new = new (temp_array + i) T();
@@ -140,7 +217,6 @@ private:
         // std::cout << _array << std::endl;
         // // DEBUG END
 
-        // T* peek = _array;
         // std::cout << "Before free" << std::endl;
         // for (int i = 0; i < _array_capacity; i++)
         // {
--- main.cpp
+++ main.cpp
@@ -1,19 +1,83 @@
 #include "dynamic_array.cpp"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
+    Array<int> a(8);
 
+    for (int i = 0; i < 10; i++)
+    {
+        a.insert(i + 1);
+    }
+    
+    // a.testm_print();
 
+    for (int i = 0; i < a.size(); i++)
+    {
+        a[i] *= 2;
+    }
+    
+    // a.testm_print();
 
+    for (auto it = a.iterator(); it.hasNext(); it.next())
+    {
+        // std::cout << "it.get()" << std::endl;
+        std::cout << it.get() << std::endl;
+    }
+    
 
-    Array<int> a(3);
-    a.insert(15);
-    a.insert(5);
-    a.insert(12);
-    a.testm_print();
-    a.insert(1, 100);
-    a.testm_print();
+    // auto it = a.iterator();
+
+    // it.test_print();
+
+    // a.insert(15);
+    // // auto temp = it.get();
+    // std::cout << "get " << it.get() << std::endl;
+    
+    // it.set(500);
+
+    // std::cout << "get " << it.get() << std::endl;
+    
+    // a.insert(5);
+    // a.insert(12);
+    // a.testm_print();
+    // a.insert(1, 100);
+    // // a.test_resize();
+    // a.testm_print();
+    // a.remove(0);
+    // a.testm_print();
+
+    // std::cout << a[2] << std::endl;
+
+    // a[2] = 222;
+    // std::cout << a[2] << std::endl;
+    // // a.remove(2);
+    // // a.testm_print();
     // a.insert(88);
+    // a.testm_print();
+    // a.insert(9, 999);
+    // a.testm_print();
+    // a.remove(0);
+    // a.testm_print();
+    // a.remove(0);
+    // a.testm_print();
+    // a.testm_print();
+    // a.insert(88);
+    // a.testm_print();
+    // a.insert(9, 555);
+    // a.testm_print();
+    // a.insert(3, 666);
+    // a.testm_print();
+
+
+
+    // a.insert(400);
+    // a.testm_print();
+    // a.remove(0);
+    // a.testm_print();
+    // a.insert(333);
+    // a.testm_print();
+    // // a.insert(88);
     // a.testm_print();
     // a.insert(9, 999);
     // // a.testm_print();
@@ -23,6 +87,8 @@ int main(int argc, char **argv)
     // a.testm_print();
     // a.insert(3, 666);
     // a.testm_print();
+
+
     // a.insert(2);
     // a.insert(15);
     // a.insert(5);
